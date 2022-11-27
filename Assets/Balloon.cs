0b4755cef087e1b0b42c0c503721a7da10271694
Mellow1213 <dpcks7282@gmail.com>
2022-11-27 16:40:15 +0900

    흔든 거리만큼 수치 올라가는 코드 작성 완료

--- Assets/Balloon.cs
+++ Assets/Balloon.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Balloon : MonoBehaviour
 {
     float hapiness = 50;
+
+    bool isWalk = false;
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            prevPos = transform.position;
+            isWalk = !isWalk;
+        }
 
+        if (isWalk)
+            Walk();
     }
 
     void Eat()
@@ -22,9 +30,23 @@ public class Balloon : MonoBehaviour
 
     }
 
+
+
+    float walkDistance = 0;
+    Vector3 prevPos;
+    float timer = 0.1f;
     void Walk()
     {
-
+        timer += Time.deltaTime;
+        if(timer > 0.1f)
+        {
+            walkDistance += Vector3.Distance(prevPos, transform.position);
+            Debug.Log("이동 거리 : " + Vector3.Distance(prevPos, transform.position));
+            timer = 0f;
+            prevPos = transform.position;
+        }
+        Debug.Log("walkDistance : " + walkDistance);
+        Debug.Log("산책 중");
     }
 
     void Shower()
