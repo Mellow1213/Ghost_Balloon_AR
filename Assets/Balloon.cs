96de95cae90045b43ee1b07acb8f9038c5b43c97
Mellow1213 <dpcks7282@gmail.com>
2022-11-27 17:15:33 +0900

    풍선 산책 -> 놀아주기로 변경

--- Assets/Balloon.cs
+++ Assets/Balloon.cs
@@ -6,7 +6,6 @@ public class Balloon : MonoBehaviour
 {
     float hapiness = 50;
 
-    bool isWalk = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,14 +14,7 @@ public class Balloon : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Alpha0))
-        {
-            prevPos = transform.position;
-            isWalk = !isWalk;
-        }
-
-        if (isWalk)
-            Walk();
+        Play();
     }
 
     void Eat()
@@ -32,21 +24,8 @@ public class Balloon : MonoBehaviour
 
 
 
-    float walkDistance = 0;
-    Vector3 prevPos;
-    float timer = 0.1f;
     void Walk()
     {
-        timer += Time.deltaTime;
-        if(timer > 0.1f)
-        {
-            walkDistance += Vector3.Distance(prevPos, transform.position);
-            Debug.Log("이동 거리 : " + Vector3.Distance(prevPos, transform.position));
-            timer = 0f;
-            prevPos = transform.position;
-        }
-        Debug.Log("walkDistance : " + walkDistance);
-        Debug.Log("산책 중");
     }
 
     void Shower()
@@ -59,8 +38,44 @@ public class Balloon : MonoBehaviour
 
     }
 
+    bool isPlay = false;
+    float walkDistance = 0;
+    Vector3 prevPos;
+    float timer = 0.1f;
+
+    const float playTimeDistance = 100f;
     void Play()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha0) && !isPlay)
+        {
+            prevPos = transform.position;
+            isPlay = true;
+        }
+
+        if (isPlay)
+        {
+            timer += Time.deltaTime;
+            if (timer > 0.1f)
+            {
+                walkDistance += Vector3.Distance(prevPos, transform.position);
+                Debug.Log("이동 거리 : " + Vector3.Distance(prevPos, transform.position));
+                timer = 0f;
+                prevPos = transform.position;
+            }
+            Debug.Log("walkDistance : " + walkDistance);
+            Debug.Log("산책 중");
+
+            if(walkDistance >= playTimeDistance)
+            {
+                walkDistance = 0;
+                isPlay = false;
+                Debug.Log("놀아주기 끝.");
+                Debug.Log("즐거움 크게 증가.");
+                Debug.Log("청결도 작게 감소.");
+                Debug.Log("포만도 중간 감소.");
+            }
+        }
+
 
     }
 
