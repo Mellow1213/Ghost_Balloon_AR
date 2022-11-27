ce68b0e9d2d9c3fde145a9afeb2dc56158cb9f47
Mellow1213 <dpcks7282@gmail.com>
2022-11-28 00:50:36 +0900

    음식 먹기 기능 구현

--- Assets/02. Scripts/Balloon.cs
+++ Assets/02. Scripts/Balloon.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Balloon : MonoBehaviour
 {
     float hapiness = 50;
     bool isIdle = true;
+    public Transform homeTransform;
 
     // Start is called before the first frame update
     void Start()
@@ -16,17 +18,41 @@ public class Balloon : MonoBehaviour
     void Update()
     {
         Play();
+        Eat();
     }
 
+
+    public Transform eatTransform;
+    bool doEat = false;
+    public GameObject food;
     void Eat()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            doEat = true;
+        }
 
+        if (doEat)
+        {
+            doEat = false;
+            StartCoroutine(DoEat());
+        }
     }
 
+    IEnumerator DoEat()
+    {
+        transform.DOMove(eatTransform.position, 2f);
 
+        yield return new WaitForSeconds(4f);
+        food.SetActive(false);
+        transform.DOMove(homeTransform.position, 2f);
+        Debug.Log("음식 섭취");
+        Debug.Log("");
+    }
 
     void Walk()
     {
+
     }
 
 
