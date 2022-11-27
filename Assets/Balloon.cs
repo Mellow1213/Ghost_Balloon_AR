6c15d6cf9be26aee96eb604795de8fbe7d8528c6
Mellow1213 <dpcks7282@gmail.com>
2022-11-27 15:42:25 +0900

    URP 넣음. 풍선 행동 함수 틀

---
+++ Assets/Balloon.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    float hapiness = 50;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void Eat()
+    {
+
+    }
+
+    void Walk()
+    {
+
+    }
+
+    void Shower()
+    {
+
+    }
+
+    void Exercise()
+    {
+
+    }
+
+    void Play()
+    {
+
+    }
+
+    void Idle()
+    {
+
+    }
+}
